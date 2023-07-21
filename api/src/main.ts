f47033554af76ea18b26ec56cbee3527dfd49ef6
Victor Alves <victormssalves@outlook.com>
2023-07-21 03:54:55 -0300

    Eu não aguento mais

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,9 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as bodyParser from 'body-parser';
-import * as fs from 'fs';
 
 async function bootstrap() {
   try {
@@ -20,13 +19,16 @@ async function bootstrap() {
         'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     });
 
-    const swaggerJson = JSON.parse(
-      fs.readFileSync(
-        '/home/victoralves/Documentos/GitHub/Lionhearth/api/swagger.json',
-        'utf8',
-      ),
-    );
-    const document = SwaggerModule.createDocument(app, swaggerJson);
+    const config = new DocumentBuilder()
+      .setTitle('Lionhearth API')
+      .setDescription(
+        'The Lionhearth API provides endpoints to manage user accounts, access game data, and interact with the Lionhearth RPG platform.',
+      )
+      .setVersion('v0.0.1')
+      .addTag('users') // Add tags for grouping related endpoints (optional)
+      .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }) // Include security scheme for JWT authorization (optional)
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('', app, document);
     console.log(`Status: 200 OK | Server running on http://localhost:${port}/`);
     await app.listen(port, '0.0.0.0');
