95316b70cb91dfb0cacc3e365c2ae37f4c52a60e
Victor Alves <victormssalves@outlook.com>
2023-07-24 01:34:38 -0300

    Update page.tsx

--- client/src/app/pt-br/auth/sign-up/page.tsx
+++ client/src/app/pt-br/auth/sign-up/page.tsx
@@ -16,34 +16,15 @@ export const metadata: Metadata = {
 export default function SignUp() {
   return (
     <>
-      <div className="container relative h-[1080px] flex-col items-center justify-center bg-zinc-900 md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="hidden relative flex-col px-10 h-full text-white bg-muted dark:border-r lg:flex">
-          <div className="absolute inset-0" />
-          <a className="flex relative z-20 items-center text-lg font-medium" href="https://archei.vercel.app">
-            <Image 
-            src={archeiWhite}
-            alt="Logo da Archei Software"
-            width={200}
-            height={100}
-            />
-          </a>
-          <div className="flex relative z-20 justify-center items-center">
-            <Image 
-            src={logo}
-            alt="Logo da Lionhearth"
-            width={550}
-            height={100}
-            />
-          </div>
-        </div>
-        <div className="mb-20 lg:px-8">
+      <div className="container relative h-[1080px] flex-col align-middle justify-center bg-zinc-900 md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
+        <div className="">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
+            <div className="flex flex-col space-y-2 text-center mt-24">
               <h1 className="text-2xl font-semibold tracking-tight text-white">
                 Cadastre-se
               </h1>
               <p className="text-sm text-white text-muted-foreground">
-              Sem precisar enfrentar dragões, crie sua conta em poucos segundos!
+              Sem precisar enfrentar dragões, crie sua conta em pouquissímos segundos!
               </p>
             </div>
             <UserSignUpForm />
