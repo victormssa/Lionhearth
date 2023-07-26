bf27f13d19b16ebe92e38933cd08e45dac8a7e4f
Victor Alves <victormssalves@outlook.com>
2023-07-25 21:30:17 -0300

    Update layout.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import Head from 'next/head';
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
 const inter = Inter({ subsets: ['latin']})
@@ -16,15 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_001b6ee02d50395822e973599a72fb1411ba/sdk.js"),(document.body || document.head).appendChild(n)();
-            `,
-          }}
-        />
-      </Head>
       <body className={inter.className}>
         {children}
       </body>
