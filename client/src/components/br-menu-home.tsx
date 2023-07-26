2e25e74244df93b771225552b77dd95cac4d54ba
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-07-26 16:35:39 -0300

    Finalizando frontend de status funcional

--- client/src/components/br-menu-home.tsx
+++ client/src/components/br-menu-home.tsx
@@ -1,14 +1,35 @@
 'use client'
+import {
+  CreditCard,
+  LifeBuoy,
+  LogOut,
+  User,
+  UserPlus,
+  Dot
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+} from "../components/ui/dropdown-menu";
 
 import { useEffect, useState } from 'react';
 import { BiSolidCartDownload } from 'react-icons/bi';
-import { FaDiceD20, FaUserFriends, FaUserPlus } from 'react-icons/fa';
+import { FaDiceD20, FaUserFriends } from 'react-icons/fa';
 import { IoNotifications, IoSettings } from 'react-icons/io5'
-import { BsPlusCircleFill} from 'react-icons/bs'
 import { MdPersonSearch } from 'react-icons/md'
 import { HiUserAdd } from 'react-icons/hi'
 import { AiOutlineSearch, AiOutlinePlus } from 'react-icons/ai';
 import { useRouter, usePathname } from "next/navigation";
+
 import Image from 'next/image';
 import logo from '../../public/assets/images/apple-touch-icon.png';
 import avatarHayato from '../../public/assets/images/hayato.png';
@@ -16,23 +37,21 @@ import avatarSho from '../../public/assets/images/Sho.png';
 import avatarLoli from './../../public/assets/images/1.png';
 import avatarHitler from './../../public/assets/images/2.jpg';
 import { Input } from "./ui/input";
-import plusSvg from '../../public/assets/images/plus-svgrepo-com.svg'
-import { createAvatar } from '@dicebear/core';
-import { identicon } from '@dicebear/collection';
 import axios, { AxiosRequestConfig } from "axios";
 import Cookies from 'js-cookie';
 import jwtDecode from "jwt-decode";
-import * as path from 'path';
 
 export default function Menu() {
   const router = useRouter();
   const [userId, setUserId] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
   const [selectedIcon, setSelectedIcon] = useState(null);
   const [contactOpen, setContactOpen] = useState(false);
   const [inputType, setInputType] = useState<string | null>(null);
   const [profileImage, setProfileImage] = useState("");
   const [pesquisarOpen, setPesquisarOpen] = useState(false);
   const [addAmigoOpen, setAddAmigoOpen] = useState(false);
+  const [status, setStatus] = useState("");
 
   const API_URL = "https://api-lionhearth.vercel.app/users"
 
@@ -54,11 +73,13 @@ export default function Menu() {
           try {
             const response = await axios.get(`${API_URL}/${userId}`, config);
             const imageBuffer = response.data.profileImage.data; // obtém o buffer de imagem do response
+            const username = response.data.username;
             const blob = new Blob([new Uint8Array(imageBuffer)], {
               type: "image/*",
             }); // cria um objeto Blob a partir do buffer
             const imageUrl = URL.createObjectURL(blob); // cria um URL para o objeto Blob
             setProfileImage(imageUrl); // define a URL como a fonte da imagem
+            setUsername(username)
           } catch (error) {
             console.error(error);
           }
@@ -131,6 +152,18 @@ export default function Menu() {
     }
   };
 
+  const getDotColor = () => {
+    switch (status) {
+      case "Online":
+        return "bg-emerald-500";
+      case "Ocupado":
+        return "bg-yellow-500";
+      case "Não-Pertubar":
+        return "bg-red-700";
+      default:
+        return "bg-emerald-500"; // Cor padrão, caso o status não corresponda a nenhum caso acima
+    }
+  };
 
   return (
     <>
@@ -158,64 +191,110 @@ export default function Menu() {
           <a href="settings" onClick={() => handleIconClick('icon5')} className={`p-1.5 hover:text-red-500 focus:outline-none transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800 ${selectedIcon === 'icon5' ? 'text-red-700' : 'text-gray-500'} ${isActive("/pt-br/settings")}`}>
             <IoSettings className='text-2xl'></IoSettings>
           </a>
-          <div className='absolute bottom-2'>
-            <button className='relative'>
-              <Image className="object-cover w-10 h-10 rounded-full border-2 border-red-700" src={profileImage} alt='' width={400} height={400} />
-              <span className="h-2 w-2 rounded-full bg-emerald-500 absolute right-0.5 ring-1 ring-white bottom-0"></span>
-            </button>
+          <div className='absolute bottom-2 '>
+            <DropdownMenu>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <button className='relative'>
+                    <Image className="object-cover w-12 h-12 rounded-full border-2 border-red-700" src={profileImage} alt='' width={400} height={400} />
+                    <span className={`h-3 w-3 rounded-full absolute right-1 ring-1 ring-white bottom-0 ${getDotColor()}`}></span>                  
+                    </button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <div className='relative'>
+                    <Image className="object-cover w-20 h-20 rounded-full border-2 border-red-700" src={profileImage} alt='' width={400} height={400} />
+                    <span className={`h-4 w-4 rounded-full ${getDotColor()} absolute right-36 ring-1 ring-white bottom-0`}></span> 
+                  </div>
+                  <DropdownMenuLabel>{username}</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <User className="mr-2 h-4 w-4" />
+                      <span>Perfil</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <CreditCard className="mr-2 h-4 w-4" />
+                      <span>Assinatura</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuSub>
+                      <DropdownMenuSubTrigger>
+                        <UserPlus className="mr-2 h-4 w-4" />
+                        <span>Status</span>
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuSubContent>
+                          <DropdownMenuItem onClick={() => setStatus("Online")}>
+                            <Dot className="h-10 w-10 text-green-500" />
+                            <span>Online</span>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem onClick={() => setStatus("Ocupado")}>
+                            <Dot className="h-10 w-10 text-yellow-500" />
+                            <span>Ocupado</span>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem onClick={() => setStatus("Não-Pertubar")}>
+                            <Dot className="h-10 w-10 text-red-500" />
+                            <span>Não pertubar</span>
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                        </DropdownMenuSubContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <LifeBuoy className="mr-2 h-4 w-4" />
+                    <span>Suporte</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <span>Sair</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </DropdownMenu>
           </div>
         </div>
-
         <div className={`h-screen ${contactOpen ? 'block' : 'hidden'} py-8 overflow-y-auto bg-zinc-900 border-zinc-950 border-l border-r sm:w-64 w-60 dark:bg-gray-900 dark:border-gray-700`}>
           <h2 className="px-5 text-lg font-medium text-white dark:text-white">Contatos ( 3 / 4 )</h2>
-
           <div className="mt-8 space-y-4">
             <button className="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-zinc-800 focus:outline-none">
               <Image className="object-cover w-8 h-8 rounded-full" src={avatarHayato} width={400} height={400} alt="" />
-
               <div className="text-left rtl:text-right">
                 <h1 className="text-sm font-medium text-white capitalize dark:text-white">Gustavo Rosado</h1>
-
                 <p className="text-xs text-gray-500 dark:text-gray-400">Offline</p>
               </div>
             </button>
-
             <button className="flex items-center w-full px-5 py-2 transition-colors duration-200 hover:bg-zinc-800 dark:hover:bg-gray-800 gap-x-2 focus:outline-none">
               <div className="relative">
                 <Image className="object-cover w-8 h-8 rounded-full" src={avatarSho} width={400} height={400} alt="" />
                 <span className="h-2 w-2 rounded-full bg-yellow-500 absolute right-0.5 ring-1 ring-white bottom-0"></span>
               </div>
-
               <div className="text-left rtl:text-right">
                 <h1 className="text-sm font-medium text-white capitalize dark:text-white">Bruno Seixas</h1>
-
                 <p className="text-xs text-gray-500 dark:text-gray-400">Ocupado</p>
               </div>
             </button>
-
             <button className="flex items-center w-full px-5 py-2 transition-colors duration-200 hover:bg-zinc-800 dark:bg-gray-800 gap-x-2 focus:outline-none">
               <div className="relative">
                 <Image className="object-cover w-8 h-8 rounded-full" src={avatarLoli} width={400} height={400} alt="" />
                 <span className="h-2 w-2 rounded-full bg-emerald-500 absolute right-0.5 ring-1 ring-white bottom-0"></span>
               </div>
-
               <div className="text-left rtl:text-right">
                 <h1 className="text-sm font-medium text-white capitalize dark:text-white">Felipe Lima</h1>
-
                 <p className="text-xs text-gray-500 dark:text-gray-400">Online</p>
               </div>
             </button>
-
-
             <button className="flex items-center w-full px-5 py-2 transition-colors duration-200 hover:bg-zinc-800 dark:hover:bg-gray-800 gap-x-2 focus:outline-none">
               <div className="relative">
                 <Image className="object-cover w-8 h-8 rounded-full" src={avatarHitler} width={400} height={400} alt="" />
                 <span className="h-2 w-2 rounded-full bg-red-700 absolute right-0.5 ring-1 ring-white bottom-0"></span>
               </div>
-
               <div className="text-left rtl:text-right">
                 <h1 className="text-sm font-medium text-white capitalize dark:text-white">Guilherme</h1>
-
                 <p className="text-xs text-gray-500 dark:text-gray-400">Não pertubar</p>
               </div>
             </button>
@@ -226,36 +305,34 @@ export default function Menu() {
                   className="text-2xl absolute top-[0.5rem] left-[10rem] cursor-pointer hover:text-red-600 text-gray-400 focus:outline-none transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800"
                 />
                 {inputType === 'pesquisar' && ( // Show the "Pesquisar" input only when inputType is 'pesquisar'
-                
                   <div className='relative'>
                     <Input
-                    placeholder="Pesquisar"
-                    type="text"
-                    autoCapitalize="none"
-                    autoCorrect="off"
-                    required
-                    className="bg-zinc-900 absolute top-[-3rem] w-60 ml-2 border-red-700">
+                      placeholder="Pesquisar"
+                      type="text"
+                      autoCapitalize="none"
+                      autoCorrect="off"
+                      required
+                      className="bg-zinc-900 absolute top-[-3rem] w-60 ml-2 border-red-700">
                     </Input>
-                    <AiOutlineSearch className='absolute bottom-[1.25rem] left-[14rem] text-gray-400 hover:text-red-500 cursor-pointer'/>
+                    <AiOutlineSearch className='absolute bottom-[1.25rem] left-[14rem] text-gray-400 hover:text-red-500 cursor-pointer' />
                   </div>
-
                 )}
                 <HiUserAdd
                   onClick={handleBiUserPlusClick}
                   className="text-2xl absolute top-[0.5rem] left-[4rem] cursor-pointer hover:text-red-600 text-gray-400 focus:outline-none transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800"
                 />
                 {inputType === 'addAmigo' && ( // Show the "Add Amigo" input only when inputType is 'addAmigo'
-                     <div className='relative'>
-                     <Input
-                     placeholder="Username"
-                     type="text"
-                     autoCapitalize="none"
-                     autoCorrect="off"
-                     required
-                     className="bg-zinc-900 absolute top-[-3rem] w-60 ml-2 border-red-700">
-                     </Input>
-                     <AiOutlinePlus className='absolute bottom-[1.25rem] left-[14rem] text-gray-400 hover:text-red-500 cursor-pointer'/>
-                   </div>
+                  <div className='relative'>
+                    <Input
+                      placeholder="Username"
+                      type="text"
+                      autoCapitalize="none"
+                      autoCorrect="off"
+                      required
+                      className="bg-zinc-900 absolute top-[-3rem] w-60 ml-2 border-red-700">
+                    </Input>
+                    <AiOutlinePlus className='absolute bottom-[1.25rem] left-[14rem] text-gray-400 hover:text-red-500 cursor-pointer' />
+                  </div>
                 )}
               </div>
             </div>
